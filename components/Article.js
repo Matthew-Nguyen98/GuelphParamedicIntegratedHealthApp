22b8b7d055d29220c47bd8d8eb681fc6d28fae93
Van1shzz <mnguyen0012@conestogac.on.ca>
2022-02-03 15:30:31 -0500

    test commit for new branch

--- components/Article.js
+++ components/Article.js
@@ -4,7 +4,11 @@ import { View, Text, StyleSheet } from "react-native";
 const Article = (props) =>{
 
     return (
+        
         <View>
+
+            {/* article title */}
+            {/* article content */}
             <Text>{props.Text}</Text>
         </View>
     )
