1188d2efe2bdd26a3154e6b1a08d1875e64b2670
Van1shzz <mnguyen0012@conestogac.on.ca>
2022-02-12 11:23:58 -0500

    fully functioning nav that doesnt appear on login

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import react from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
 import ContactScreen  from './screens/ContactScreen';
@@ -9,25 +10,29 @@ import ResourceScreen from './screens/ResourceScreen';
 import AssessmentScreen from './screens/Assessment';
 import MedicalDirectiveScreen from './screens/MedicalDirective';
 import FormsScreen from './screens/Forms';
-import * as React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import SideNav from './Routes/SideNav';
 
-const Drawer = createDrawerNavigator();
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+// function Root() {
+//   return (
+//     <Drawer.Navigator>
+//       <Drawer.Screen name="Home" component={HomeScreen} />
+//       <Drawer.Screen name="Contact" component={ContactScreen} />
+//       <Drawer.Screen name="Resources" component={ResourceScreen} />
+//       <Drawer.Screen name="Assessments" component={AssessmentScreen} />
+//       <Drawer.Screen name="Forms" component={FormsScreen} />
+//     </Drawer.Navigator>
+//   );
+// }
+
 export default function App() {
-
-
   return (
     <NavigationContainer>
-    <Drawer.Navigator initialRouteName='Login'>
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Drawer.Screen name="Home" component={HomeScreen} />
-      <Drawer.Screen name="Contact" component={ContactScreen} />
-      <Drawer.Screen name="Resource" component={ResourceScreen} />
-      <Drawer.Screen name="Assessment" component={AssessmentScreen} />
-      <Drawer.Screen name="MedicalDirective" component={MedicalDirectiveScreen} />
-      <Drawer.Screen name="Forms" component={FormsScreen} />
-    </Drawer.Navigator>
+        <Stack.Navigator>
+    <Stack.Screen name="Login" component={LoginScreen}/>
+    <Stack.Screen name="Home" component={SideNav} options={{ headerShown: false }} />
+  </Stack.Navigator>
   </NavigationContainer>
   );
 }
