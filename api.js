ce296c06f998a07cd357734249c84ef35dc6f9bd
Tan Cuong Luong <Tluong9057@conestogac.on.ca>
2022-03-11 11:49:47 -0500

    api 2nd

--- api.js
+++ api.js
@@ -8,6 +8,10 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-
+app.use(express.json())
 app.use('/api',contactRoutes.routes);
+app.get("/", (req, res) => {
+    res.send("API running fine");
+  });
+    
 app.listen(config.port, ()=> console.log('Express Server listening on http://localhost:' + config.port));
